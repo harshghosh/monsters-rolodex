ac552a18bb18bad94d3f2436ee2e1e70128abc71
Harsh Ghosh <harsh.ghosh@quest-global.com>
2021-03-15 13:35:25 +0530

    adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const SearchBox = ({placeholder,handleChange}) => (
+
+    <input type="search" 
+    placeholder={placeholder}
+    onChange={handleChange}/>
+)
